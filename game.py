f2e42b6249e489e66263f65895de3e89b4880281
JohnSV18 <69915500+JohnSV18@users.noreply.github.com>
2021-03-23 20:24:52 -0400

    added stats on the top corner

--- game.py
+++ game.py
@@ -8,17 +8,17 @@ screen_width, screen_height = 900, 600
 screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption("Mathtris")
 frames_per_second = 60
+# velocity of basket moving side to side
 velocity = 5
+# color of balls that fall from above
 ball_color = (random.randint(0,150),random.randint(100, 200), 0)
 
-
 black = (0, 0, 0)
-
+# font size and style
 health_font = pygame.font.SysFont('comicsans', 40)
-
+# image of basket 
 basket_image = pygame.image.load(os.path.join('assets', 'cart.jpg'))
-# basket = pygame.transform.scale(basket_image, (55,40))
-# Basket class which hold the attributes to the basket that will be grabbing the objects falling from above
+
 
 
 class Ball():
@@ -32,22 +32,14 @@ class Ball():
 
     def set_position_x(self):
         self.position_x = random.randint(100, 900)
-    
-    # def handle_balls(self):
-    #     for ball in balls:
-    #         if ball.colliderect(circle_basket):
-    #             balls.remove(ball)
-            
+
 
 
 class Basket():
     def __init__(self):
         self.height = 55
-        self.width = 40
-        self.score = 0
-        self.positions = [(int(screen_height/2), int(screen_height/2))]
-    
-# the reset method will reset the score to 0 and also place the cart right at the center at the starting points
+        self.width = 40    
+
 
     def show_basket(self):
         basket_image = pygame.image.load(os.path.join('assets', 'cart.jpg'))
@@ -60,15 +52,26 @@ def draw_window(circle_basket, x, y, balls, health, points, target_score):
     b = Basket()
     screen.fill((255,255,255))
     screen.blit(b.show_basket(), (x, y))
+    target_point_text = health_font.render("Target Point: " + str(target_score), 1, black)
+    screen.blit(target_point_text, (680, 25))
     health_text = health_font.render("Health: " + str(health), 1, black)
-    screen.blit(health_text, )
+    screen.blit(health_text, (680, 50))
+    score_text=health_font.render("Score: " + str(points), 1, black)
+    screen.blit(score_text, (680, 75))
+
 
 
     for ball in balls:
         pygame.draw.circle(screen, ball.ball_color, (ball.position_x, ball.position_y), ball.width, ball.height)
         if ball.position_y > screen_height:
             pygame.draw.circle(screen, ball.ball_color, (ball.position_x, ball.position_y), ball.width, ball.height)
-
+    if points == target_score:
+        won_text = health_font.render("You Win!", 1, black)
+        screen.blit(won_text, (screen_width/2, screen_height/2))
+    elif health <= 0:
+        lost_health_text = health_font.render("You Lose!", 1, black)
+        screen.blit(lost_health_text, (screen_width/2, screen_height/2))
+    
 
     pygame.display.update()
     
@@ -76,7 +79,7 @@ def draw_window(circle_basket, x, y, balls, health, points, target_score):
 
 
 def main():
-    target_score = random.randint(7,17)
+    target_score = random.randint(7,11)
     health = 10
     points = 0
     b = Basket()
@@ -86,45 +89,46 @@ def main():
     clock = pygame.time.Clock()
     game_is_running = True
     balls = []
-    
-    circle_y_position = 40
+
     while game_is_running:
         clock.tick(frames_per_second)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_is_running = False
            
-        if health <= 0:
-            win_text = "You Lose!"
-        if points == target_score:
-            win_text = "You Win!"
-        elif points > target_score:
-            win_text = "You Lose!"
-            
-        
-
-        while len(balls) < 4:
+        # if health <= 0:
+        #     for ball in balls:
+        #         ball.position_y = 0
+        #     win_text = "You Lose!"
+        #     print(win_text)
+        # if points == target_score:
+        #     win_text = "You Win!"
+        #     print(win_text)
+      
+        while len(balls) < 3:
             ball = Ball(balls.count)
             ball.set_position_x()
             balls.append(ball)
-        
-
     
         for ball in balls:
-            ball.position_y += random.randint(2,6)
+            if health > 0:
+                ball.position_y += random.randint(2,6)
+            else:
+                ball.position_y = -10
+            if points == target_score:
+                ball.position_y = -10
+                
 
             ball_tuple = (ball.position_x, ball.position_y, ball.width, ball.height)
+            
             print(f"ball number {ball.number} position {ball.position_x}")
 
-            if pygame.Rect.colliderect(circle_basket, ball_tuple):
+            if pygame.Rect.collidelist(circle_basket, ball_tuple):
                 points += 1
                 balls.remove(ball)
             if ball.position_y > screen_height:
                 health -= 1                
                 balls.remove(ball)
-
-                
-
 
 
         keys_pressed = pygame.key.get_pressed()
